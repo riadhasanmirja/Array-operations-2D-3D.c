85711191f948849b937843a56da509d609550bd5
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2026-04-17 22:05:26 +0600

    Create sum of 5 number.c

---
+++ sum of 5 number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+          int num[5]={1,4,7,5,9},sum=0,i;
+
+          for(i=0; i<5; i++)
+
+          {
+                    sum = sum + num[i] ;
+          }
+
+          printf("%d",sum);
+
+          return 0;
+}
